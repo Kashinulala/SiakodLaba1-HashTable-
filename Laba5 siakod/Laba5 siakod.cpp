6f57545eb16d1940b8b544349a83b456656d291c
Kashinulala <karavatskydanil@gmail.com>
2024-04-03 12:18:13 +1000

    1111

--- Laba5 siakod/Laba5 siakod.cpp
+++ Laba5 siakod/Laba5 siakod.cpp
@@ -12,11 +12,18 @@ int main() {
 	Employee emp1("Avanov", "A.I.", d1, "Manager", 3);
 	Employee emp11("Vonava", "I.A.", d1, "Manager", 3);
 	Employee emp2("Sidorov", "S.A.", d2, "Administrator", 4);
+	Employee emp3 = emp2;
 
 	cout << "_____________________________________________" << endl << endl;
 	cout << ht;
 
 	//Добавление в пустую таблицу
+	ht.AddRecord(emp1);
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	//Добавление существующей записи
 	ht.AddRecord(emp1);
 
 	cout << "_____________________________________________" << endl << endl;
@@ -28,8 +35,6 @@ int main() {
 	cout << "_____________________________________________" << endl << endl;
 	cout << ht;
 
-	//Удаление записи с коллизией
-	ht.DelRecord(emp11);
 
 	for (int i = 0; i < 3; i++) {
 		emp1.surname[0] += 1;
@@ -45,7 +50,70 @@ int main() {
 	cout << "_____________________________________________" << endl << endl;
 	cout << ht;
 
-	for (int i = 0; i < 5; i++) {
+	//Добавление записи в заполненную на 80% таблицу
+
+	ht.AddRecord(emp3);
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	for (int i = 0; i < 2; i++) {
+		ht.DelRecord(emp1);
+		emp1.surname[0] -= 1;
+		emp1.initials[0] -= 1;
+		emp1.dob.yy += 1;
+		ht.DelRecord(emp2);
+		emp2.surname[3] -= 2;
+		emp2.initials[0] -= 2;
+		emp2.dob.yy += 1;
+	}
+	ht.DelRecord(emp1);
+	emp1.surname[0] -= 1;
+	emp1.initials[0] -= 1;
+	emp1.dob.yy += 1;
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	//Удаление из таблицы заполненной на 20% (N = 10; k = 0, 75)
+
+	ht.DelRecord(emp3);
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	ht.DelRecord(emp2);
+	emp2.surname[3] -= 2;
+	emp2.initials[0] -= 2;
+	emp2.dob.yy += 1;
+	
+	for (int i = 0; i < 2; i++) {
+		emp1.surname[0] += 1;
+		emp1.initials[0] += 1;
+		emp1.dob.yy -= 1;
+		ht.AddRecord(emp1);
+		emp2.surname[3] += 2;
+		emp2.initials[0] += 2;
+		emp2.dob.yy -= 1;
+		ht.AddRecord(emp2);
+	}
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	//Добавление записи с несколькими коллизиями 
+	Employee emp4("Vaanov", "A.I.", d1, "Manager", 3);
+	ht.AddRecord(emp4);
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	//Удаление существующей записи (по второй хеш-функции)
+	ht.DelRecord(emp4);
+
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	for (int i = 0; i < 2; i++) {
 		ht.DelRecord(emp1);
 		emp1.surname[0] -= 1;
 		emp1.initials[0] -= 1;
@@ -58,17 +126,19 @@ int main() {
 
 	cout << "_____________________________________________" << endl << endl;
 	cout << ht;
-	
-	for (int i = 0; i < 5; i++) {
-		emp1.surname[0] += 1;
-		emp1.initials[0] += 1;
-		emp1.dob.yy -= 1;
-		ht.AddRecord(emp1);
-		emp2.surname[3] += 2;
-		emp2.initials[0] += 2;
-		emp2.dob.yy -= 1;
-		ht.AddRecord(emp2);
-	}
+
+	if (ht.SearchRec(emp11) == true) cout << "1";
+	if (ht.SearchRec(emp1) == true) cout << "1";
+	if (ht.SearchRec(emp2) == false) cout << "1";
+
+	if (ht.DelRecord(emp2) == 0) cout << "1";
+
+	cout << endl;
+	ht.DelRecord(emp1);
+	cout << "_____________________________________________" << endl << endl;
+	cout << ht;
+
+	ht.DelRecord(emp11);
 	cout << "_____________________________________________" << endl << endl;
 	cout << ht;
 
